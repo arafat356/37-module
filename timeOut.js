20757517eb467474e870a077d14ffad3c06ca097
arafat356 <ahmedarafatft@gmail.com>
2022-02-26 01:55:38 +0600

    nothing

---
+++ timeOut.js
@@ -0,0 +1,15 @@
+function doshomeThing() {
+    console.log('i am doing something00000000');
+}
+console.log('first: ami tumar age');
+console.log('second: there is something');
+// setTimeout(doshomeThing, 5000); this is time set to output
+setTimeout(function() {
+    console.log('i am using javaScript');
+}, 5000);
+setTimeout(() => {
+    console.log('Exploring MDN articles');
+}, 4000);
+console.log('third: ami tumar gulpo hobo');
+console.log('fourth: kno meg ashe hriyo akashe');
+console.log('five: ghor chada akash hoye tumai p*** chai');
